8ec225fed824bb94628da495582e1f5ef8ddbd88
Ishti97 <ishtiakrahman15@gmail.com>
2021-07-01 20:05:25 +0600

    Updated admin

--- core/test/test_models.py
+++ core/test/test_models.py
@@ -28,7 +28,7 @@ class ModelTests(TestCase):
 
     def test_create_new_superuser(self):
         """Test creating a new superuser"""
-        user = get_user_model().objects.create_user('test@gmail.com', 'test123')
+        user = get_user_model().objects.create_superuser('test@gmail.com', 'test123')
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
