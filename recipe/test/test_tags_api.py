380ef339351d77a6dcf55f226055ddc54d6c461d
Ishti97 <ishtiakrahman15@gmail.com>
2021-07-05 20:52:11 +0600

    Ingredient feature added

--- recipe/test/test_tags_api.py
+++ recipe/test/test_tags_api.py
@@ -24,6 +24,7 @@ class PublicTagsApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
+
 class PrivateTagsApiTests(TestCase):
     """Test the authorized user tags api"""
 
